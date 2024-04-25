90e031c669c768a46c2375a93dafef332a7feb34
TothZsofia <tzsofia07@gmail.com>
2024-04-25 12:11:44 +0200

    Add project files.

---
+++ Weather/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weather
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hozzaadas_Click(object sender, RoutedEventArgs e)
+        {
+            if (textbox.Text != "")
+            {
+                listbox.Items.Add(textbox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Valamit írj be!");
+            }
+        }
+
+        private void Törlés_Click(object sender, RoutedEventArgs e)
+        {
+            if(listbox.SelectedItem != null)
+            {
+                listbox.Items.Remove(listbox.SelectedItem);
+            }
+        }
+    }
+}
